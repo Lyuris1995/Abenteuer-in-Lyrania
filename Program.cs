7a8761755f6baa85e428e5322e7f9e9f009712e2
Julian Schmidt <Js4595@hotmail.com>
2023-04-19 16:57:00 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abenteuer_in_Lyrania
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Willkommen, Abenteurer!");
+            Console.WriteLine("Du wirst gleich nach Lyrania reisen, mach dich bereit.");
+
+            string playerName = "";
+            Console.WriteLine("Bitte gib deinen Namen ein: ");
+            playerName = Console.ReadLine();
+            Console.WriteLine($"Deine Name lautet nun: {playerName}");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
