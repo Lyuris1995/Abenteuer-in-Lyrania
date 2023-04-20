4edfd232887b10ad5cd3908a0dc8db0e6296c259
Julian Schmidt <Js4595@hotmail.com>
2023-04-20 14:02:58 +0200

    Titel und HUD hinzugefügt.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,19 @@ namespace Abenteuer_in_Lyrania
     {
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string title = @"                                                                                                                                            
+  ,---.  ,--.                    ,--.                                   ,--.            ,--.                                   ,--.         
+ /  O  \ |  |-.  ,---. ,--,--, ,-'  '-. ,---. ,--.,--. ,---. ,--.--.    `--',--,--,     |  |   ,--. ,--.,--.--. ,--,--.,--,--, `--' ,--,--. 
+|  .-.  || .-. '| .-. :|      \'-.  .-'| .-. :|  ||  || .-. :|  .--'    ,--.|      \    |  |    \  '  / |  .--'' ,-.  ||      \,--.' ,-.  | 
+|  | |  || `-' |\   --.|  ||  |  |  |  \   --.'  ''  '\   --.|  |       |  ||  ||  |    |  '--.  \   '  |  |   \ '-'  ||  ||  ||  |\ '-'  | 
+`--' `--' `---'  `----'`--''--'  `--'   `----' `----'  `----'`--'       `--'`--''--'    `-----'.-'  /   `--'    `--`--'`--''--'`--' `--`--' 
+                                                                                               `---'                                        ";
+
+            Console.WriteLine(title);
+            Console.ResetColor();
+            Console.WriteLine("Drücke Enter zum starten.");
+            Console.ReadLine();
             Game.StartGame();
             Console.ReadLine();
         }
@@ -30,43 +43,77 @@ namespace Abenteuer_in_Lyrania
 
         public static void StartGame()
         {
+            string Location = "Startzone";
+            int Turn = 0;
+            int Gold = 0;
+
+            Console.Title = "Abenteuer in Lyrania";
+
+            while (Turn < 20)
+            {
+                Turn++; Gold++;
+                HUD(playerName, Location, Turn, Gold);
+            }
+            
             Console.WriteLine("Willkommen, Abenteurer!");
             Console.WriteLine("Du wirst gleich nach Lyrania reisen, mach dich bereit.");
-            PlayerName();
+            PlayerClass();
             Console.WriteLine("Plötzlich hörst du eine rauschende Stimme ertönen. Es ist Dunkel und du siehst nur den Umriss dieser Person. Es sieht so aus, als ob sie Flügel habe.");
             SystemDialog("\"Du wirst nun nach Lyrania beschworen. Dein Abenteuer beginnt!\"");
             Console.WriteLine("Dir wird schwarz vor Augen... Als du aufwachst, findest du dich auf einer riesigen großen Wiese unter einem Baum wieder.");
+            Console.Clear();
             Console.WriteLine("Du siehst einen jungen Mann, welcher gerade dabei ist, Samen zu säen. Er sieht dich ebenfalls und kommt auf dich zu.");
-            StrangerDialog("Servus! Hab' dich gar nich' geseh'n. Wo kommst'n du her? Wenn du hier bleiben willst schnapp dir ein paar Samen, oder verzieh dich!", "green");
+            StrangerDialog("\"Servus! Hab' dich gar nich' geseh'n. Wo kommst'n du her? Wenn du hier bleiben willst schnapp dir ein paar Samen, oder verzieh dich!\"", "green");
 
         }
 
-        static void PlayerName()
+        static void PlayerClass()
         {
             Console.WriteLine("Bitte gib deinen Namen ein: ");
             playerName = Console.ReadLine();
             Console.WriteLine($"Deine Name lautet nun: {playerName}");
+
+            
         }
 
         static void SystemDialog(string message)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.WriteLine(message);
             Console.ResetColor();
         }
 
         static void StrangerDialog(string message, string color)
         {
-            if (color == "red")
-            { Console.ForegroundColor = ConsoleColor.Red; }
-            else if (color == "green")
-            { Console.ForegroundColor = ConsoleColor.Green; }
-            else if (color == "yellow")
-            { Console.ForegroundColor = ConsoleColor.Yellow; }
-            else
-            { Console.ForegroundColor = ConsoleColor.White; }
-
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+
+        public static void HUD(string player, string location, int turn, int gold)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("=============================================================");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("    " + player);
+            Console.ResetColor();
+            Console.Write(" :: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(location);
+            Console.ResetColor();
+            Console.Write(" :: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("turn " + turn + " /20");
+            Console.ResetColor();
+            Console.Write(" :: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(gold + " gold\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("=============================================================\n\n\n\n");
             Console.ResetColor();
         }
     }
