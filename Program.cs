c844943dd6bac7b6a6c7a0464af6c73ab06ed584
Julian Schmidt <Js4595@hotmail.com>
2023-04-20 11:05:17 +0200

    SystemDialog und StrangerDialog hinzugefügt.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * [Your Title]
+ * by Your Name, Date
+ *  
+ * This work is a derivative of 
+ * "C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+ * https://creativecommons.org/licenses/by/4.0/
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +19,60 @@ namespace Abenteuer_in_Lyrania
     {
         static void Main()
         {
+            Game.StartGame();
+            Console.ReadLine();
+        }
+    }
+
+    public static class Game
+    {
+        static string playerName = "";
+
+        public static void StartGame()
+        {
             Console.WriteLine("Willkommen, Abenteurer!");
             Console.WriteLine("Du wirst gleich nach Lyrania reisen, mach dich bereit.");
+            PlayerName();
+            Console.WriteLine("Plötzlich hörst du eine rauschende Stimme ertönen. Es ist Dunkel und du siehst nur den Umriss dieser Person. Es sieht so aus, als ob sie Flügel habe.");
+            SystemDialog("\"Du wirst nun nach Lyrania beschworen. Dein Abenteuer beginnt!\"");
+            Console.WriteLine("Dir wird schwarz vor Augen... Als du aufwachst, findest du dich auf einer riesigen großen Wiese unter einem Baum wieder.");
+            Console.WriteLine("Du siehst einen jungen Mann, welcher gerade dabei ist, Samen zu säen. Er sieht dich ebenfalls und kommt auf dich zu.");
+            StrangerDialog("Servus! Hab' dich gar nich' geseh'n. Wo kommst'n du her? Wenn du hier bleiben willst schnapp dir ein paar Samen, oder verzieh dich!", "green");
 
-            string playerName = "";
+        }
+
+        static void PlayerName()
+        {
             Console.WriteLine("Bitte gib deinen Namen ein: ");
             playerName = Console.ReadLine();
             Console.WriteLine($"Deine Name lautet nun: {playerName}");
-            
-
-            Console.ReadLine();
         }
+
+        static void SystemDialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void StrangerDialog(string message, string color)
+        {
+            if (color == "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (color == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (color == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+    class Item
+    {
+
     }
 }
